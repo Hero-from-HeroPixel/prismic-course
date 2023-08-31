5c0c28836460e662c5a333b2223de954b97380b7
Jean-Jacques Grobler <groblersales@gmail.com>
2023-08-31 15:52:03 +0200

    feat: Metadata and fallback metadata

--- src/prismicio.ts
+++ src/prismicio.ts
@@ -1,6 +1,6 @@
-import * as prismic from "@prismicio/client";
-import * as prismicNext from "@prismicio/next";
-import config from "../slicemachine.config.json";
+import * as prismic from '@prismicio/client';
+import * as prismicNext from '@prismicio/next';
+import config from '../slicemachine.config.json';
 
 /**
  * The project's Prismic repository name.
@@ -13,15 +13,15 @@ export const repositoryName = config.repositoryName;
  * {@link https://prismic.io/docs/route-resolver#route-resolver}
  */
 // TODO: Update the routes array to match your project's route structure.
-const routes: prismic.ClientConfig["routes"] = [
-  {
-    type: "homepage",
-    path: "/",
-  },
-  {
-    type: "page",
-    path: "/:uid",
-  },
+const routes: prismic.ClientConfig['routes'] = [
+	// {
+	//   type: "homepage",
+	//   path: "/",
+	// },
+	// {
+	//   type: "page",
+	//   path: "/:uid",
+	// },
 ];
 
 /**
@@ -31,20 +31,20 @@ const routes: prismic.ClientConfig["routes"] = [
  * @param config - Configuration for the Prismic client.
  */
 export const createClient = (config: prismicNext.CreateClientConfig = {}) => {
-  const client = prismic.createClient(repositoryName, {
-    routes,
-    fetchOptions:
-      process.env.NODE_ENV === "production"
-        ? { next: { tags: ["prismic"] }, cache: "force-cache" }
-        : { next: { revalidate: 5 } },
-    ...config,
-  });
+	const client = prismic.createClient(repositoryName, {
+		routes,
+		fetchOptions:
+			process.env.NODE_ENV === 'production'
+				? { next: { tags: ['prismic'] }, cache: 'force-cache' }
+				: { next: { revalidate: 5 } },
+		...config,
+	});
 
-  prismicNext.enableAutoPreviews({
-    client,
-    previewData: config.previewData,
-    req: config.req,
-  });
+	prismicNext.enableAutoPreviews({
+		client,
+		previewData: config.previewData,
+		req: config.req,
+	});
 
-  return client;
+	return client;
 };
